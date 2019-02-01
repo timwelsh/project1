b61901ae02c85d954ca25bdb53e1b2431a8e6a43
Tim Welsh <tim@timwelsh.com>
2019-02-01 13:25:37 -0500

    add sound/title to object

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,88 +5,196 @@ var timer;
 var data = {
     array: [{
         arrayPosition : "0",
+        title: "Sun Sonification",
         wiki: "Plasma_(physics)",
         nasa: "Plasma",
-        imageNumber: "8"
+        imageNumber: "8",
+        sound: "01.wav"
         },
         {
         arrayPosition : "1",
+        title: "Sounds of Saturn: Enceladus Moon Hiss",
         wiki: "",
         nasa: "",
-        imageNumber: ""
+        imageNumber: "",
+        sound: "02.mp3"
         },
         {
         arrayPosition : "2",
+        title: "Voyager Plasma Sounds",
         wiki: "",
         nasa: "",
-        imageNumber: ""
+        imageNumber: "",
+        sound: "03.mp3"
         },
         {
         arrayPosition : "3",
+        title: "Cassini Shields Up",
         wiki: "",
         nasa: "",
-        imageNumber: ""
+        imageNumber: "",
+        sound: "04.mp3"
         },
         {
         arrayPosition : "4",
+        title: "Juno: Crossing Jupiter's Bow Shock",
         wiki: "",
         nasa: "",
-        imageNumber: ""
+        imageNumber: "",
+        sound: "05.mp3"
         },
         {
         arrayPosition : "5",
+        title: "Beware of Jupiter’s Largest Moon Ganymede",
         wiki: "Ganymede_(moon)",
         nasa: "Ganymede",
-        imageNumber: "8"
+        imageNumber: "8",
+        sound: "06.mp3"
         },  
         {
         arrayPosition : "6",
+        title: "Juno: Entering Jupiter's Magnetosphere",
         wiki: "Bow_shocks_in_astrophysics#Around_the_Earth",
         nasa: "Jupiter",
-        imageNumber: "58"
+        imageNumber: "58",
+        sound: "07.mp3"
         },  
         {
         arrayPosition : "7",
+        title: "Kepler: Star KIC12268220C Light Curve Waves to Sound",
         wiki: "",
         nasa: "",
-        imageNumber: ""
+        imageNumber: "",
+        sound: "08.mp3"
         },  
         {
         arrayPosition : "8",
+        title: "Kepler: Star KIC7671081B Light Curve Waves to Sound",
         wiki: "Cassini%E2%80%93Huygens",
         nasa: "Cassini",
-        imageNumber: "35"
+        imageNumber: "35",
+        sound: "09.mp3"
         },  
         {
         arrayPosition : "9",
+        title: "Voyager: Lightning on Jupiter",
         wiki: "",
         nasa: "",
-        imageNumber: ""
+        imageNumber: "",
+        sound: "10.mp3"
         },  
         {
         arrayPosition : "10",
+        title: "Stardust: Passing Comet Tempel 1",
         wiki: "",
         nasa: "",
-        imageNumber: ""
+        imageNumber: "",
+        sound: "11.mp3"
         },  
         {
         arrayPosition : "11",
+        title: "Voyager: Interstellar Plasma Sounds",
         wiki: "",
         nasa: "",
-        imageNumber: ""
+        imageNumber: "",
+        sound: "12.mp3"
         },  
         {
         arrayPosition : "12",
+        title: "Cassini: Saturn Radio Emissions #2",
         wiki: "",
         nasa: "",
-        imageNumber: ""
+        imageNumber: "",
+        sound: "13.mp3"
         },  
         {
         arrayPosition : "13",
+        title: "Chorus Radio Waves within Earth's Atmosphere",
         wiki: "",
         nasa: "",
-        imageNumber: ""
+        imageNumber: "",
+        sound: "14.mp3"
+        },
+        {
+        arrayPosition : "14",
+        title: "Cassini: Saturn Radio Emissions #1",
+        wiki: "",
+        nasa: "",
+        imageNumber: "",
+        sound: "15.mp3"
         },  
+        {
+        arrayPosition : "15",
+        title: "Cassini: Enceladus Sound",
+        wiki: "",
+        nasa: "",
+        imageNumber: "",
+        sound: "16.mp3"
+        }, 
+        {
+        arrayPosition : "16",
+        title: "Plasmaspheric Hiss",
+        wiki: "",
+        nasa: "",
+        imageNumber: "",
+        sound: "17.mp3"
+        }, 
+        {
+        arrayPosition : "17",
+        title: "Plasmawaves - Chorus",
+        wiki: "",
+        nasa: "",
+        imageNumber: "",
+        sound: "18.mp3"
+        }, 
+        {
+        arrayPosition : "18",
+        title: "Whistler Waves",
+        wiki: "",
+        nasa: "",
+        imageNumber: "",
+        sound: "19.mp3"
+        }, 
+        {
+        arrayPosition : "19",
+        title: "Jupiter Sounds 2001",
+        wiki: "",
+        nasa: "",
+        imageNumber: "",
+        sound: "20.mp3"
+        }, 
+        {
+        arrayPosition : "20",
+        title: "Ultra Cold Liquid Helium-3",
+        wiki: "",
+        nasa: "",
+        imageNumber: "",
+        sound: "21.mp3"
+        }, 
+        {
+        arrayPosition : "21",
+        title: "Radar Echoes From Titan's Surface",
+        wiki: "",
+        nasa: "",
+        imageNumber: "",
+        sound: "22.mp3"
+        }, 
+        {
+        arrayPosition : "22",
+        title: "Speeding Through Titan's Haze",
+        wiki: "",
+        nasa: "",
+        imageNumber: "",
+        sound: "23.mp3"
+        }, 
+        {
+        arrayPosition : "23",
+        title: "Scientists Find That Saturn's Rotation Period Is A Puzzle",
+        wiki: "",
+        nasa: "",
+        imageNumber: "",
+        sound: "24.mp3"
+        }, 
     ]
     
 }
