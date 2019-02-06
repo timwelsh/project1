4ca51266c407de026ab1e2f27a1b75233d7e8575
leaf-junkie <ragsdale.jar@gmail.com>
2019-02-05 19:32:38 -0500

    begin adding all ten images and sounds to all.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,58 +4,58 @@
 $(document).ready(function() {
     // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyC2u_ttl0dFV88Dyux2lNnGa-cWwFj4nVk",
-    authDomain: "space-sounds.firebaseapp.com",
-    databaseURL: "https://space-sounds.firebaseio.com",
-    projectId: "space-sounds",
-    storageBucket: "space-sounds.appspot.com",
-    messagingSenderId: "811578548981"
+    apiKey: 'AIzaSyC2u_ttl0dFV88Dyux2lNnGa-cWwFj4nVk',
+    authDomain: 'space-sounds.firebaseapp.com',
+    databaseURL: 'https://space-sounds.firebaseio.com',
+    projectId: 'space-sounds',
+    storageBucket: 'space-sounds.appspot.com',
+    messagingSenderId: '811578548981'
   };
   firebase.initializeApp(config);
 
   // Create a variable to reference the database
   var database = firebase.database();
   var counter2 = 0;
-  var name = "";
+  var name = '';
   var correct = 0;
   var incorrect = 0;
   var newPostKey = firebase.database().ref().child('posts').push().key;
 
 //TODO: Need to fix the firebase reference to the specifically saved record
-  database.ref().on("child_added", function(snapshot) {
-        $("#player").text(snapshot.val().name).addClass("has-text-white");
-        $("#correct").text(snapshot.val().correct).addClass("has-text-white");
-        $("#incorrect").text(snapshot.val().incorrect).addClass("has-text-white");
+  database.ref().on('child_added', function(snapshot) {
+        $('#player').text(snapshot.val().name).addClass('has-text-white');
+        $('#correct').text(snapshot.val().correct).addClass('has-text-white');
+        $('#incorrect').text(snapshot.val().incorrect).addClass('has-text-white');
 
     }, function(errorObject) {
-        console.log("Error: " + errorObject.code);
+        console.log('Error: ' + errorObject.code);
     });
 
-    $('#login-btn').on("click", function() {
-         var name = $("#name-input").val().trim();
+    $('#login-btn').on('click', function() {
+         var name = $('#name-input').val().trim();
 
     });  
 
     // When use hovers over an image, create overlay
-    $(".image").mouseover(function(hover) {
+    $('.image').mouseover(function(hover) {
     });
 
     var clickCounter;
 
     // When user clicks an image, pull up modal
-    $(".image").on("click", function(event) {
+    $('.image').on('click', function(event) {
         var userChoice = this.id;
         if (userChoice === answer) {
             correct++;
-            $(".modal-card-head").addClass("has-background-success");
-            $(".modal-card-title").text("Correct!").addClass("has-text-white");
+            $('.modal-card-head').addClass('has-background-success');
+            $('.modal-card-title').text('Correct!').addClass('has-text-white');
         }
         else {
             incorrect++;
-            $(".modal-card-head").addClass("has-background-danger");
-            $(".modal-card-title").text("Incorrect").addClass("has-text-white");
+            $('.modal-card-head').addClass('has-background-danger');
+            $('.modal-card-title').text('Incorrect').addClass('has-text-white');
         }
-        $(".modal").addClass("is-active");
+        $('.modal').addClass('is-active');
         clickCounter++
 
         if (clickCounter > 10) {
@@ -63,15 +63,15 @@ $(document).ready(function() {
         }
     });
 
-        // console.log (imageArray.length + " length ");
+        // console.log (imageArray.length + ' length ');
         // MODAL BUTTONS: close modal on button click or key press
-        $(".modal_button").on("click", function(e) {
-            $(".modal").removeClass("is-active");
+        $('.modal_button').on('click', function(e) {
+            $('.modal').removeClass('is-active');
             total = correct + incorrect;
-            name = "Tim";  //TODO: temporarily in here until TW fixes the firebase reference
+            name = 'Tim';  //TODO: temporarily in here until TW fixes the firebase reference
             // Game over logic
             if (total === 2) {
-                window.location = "gameover.html";
+                window.location = 'gameover.html';
                 endOfGame(correct, incorrect, name);
             } else {
                 display();
@@ -79,9 +79,10 @@ $(document).ready(function() {
             }
         });
 
-        $(window).on("keydown", function(e) {
+        // TODO: need the keypress to display next set of images
+        $(window).on('keydown', function(e) {
             if (e.keyCode === 13 || e.keyCode === 27) {
-                $(".modal").removeClass("is-active");
+                $('.modal').removeClass('is-active');
             }
         });
 
@@ -104,10 +105,14 @@ $(document).ready(function() {
             name: name
         };
         var updates = {};
-        console.log(newPostKey + " key ")
+        console.log(newPostKey + ' key ')
         updates[newPostKey] = postData;
-        console.log (name + " " + correct + " " + incorrect);
+        console.log (name + ' ' + correct + ' ' + incorrect);
         return firebase.database().ref().update(updates);
 
     }
+
+    // Populate all.html with our 10 images and sounds
+    $('.image-audio-wrapper').append
 });
+
